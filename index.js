23f808d7fb852d00a10e87a657671451c597acf9
yannick101693 <yannick101693@outlook.com>
2017-12-18 06:25:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 // write your code below!
-function happyHolidays(string){
+function happyHolidays(string)
+{
   return ('Happy holidays!')
 }
-function happyHolidaysTo(name){
+function happyHolidaysTo(name)
+{
   return ( `Happy holidays, ${name}!`)
+}
+function happyHolidayTo(holiday, name)
+{
+  return ('Happy ${holiday}, ${name}!')
 }
